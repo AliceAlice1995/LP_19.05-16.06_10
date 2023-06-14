fde5bef9989bb554b409ec59e6e6087bcfd8af89
Alisa Lubinets <lubinets.alisa@gmail.com>
2023-06-14 21:06:41 +0300

    Added Task28

---
+++ Task28/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number <= 0)
+{
+    Console.WriteLine("Введено некорректное число");
+    return;
+}
+
+int Faktorial(int num) // num = number
+{
+    int fak = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        checked 
+        {
+            fak *= i;
+        }
+    }
+    return fak;
+}
+
+
+int faktorial = Faktorial(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} = {faktorial}");
