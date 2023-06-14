64ff0b1fddc8c6212b5c588d99aef48bb10e92d2
Alisa Lubinets <lubinets.alisa@gmail.com>
2023-06-14 19:53:31 +0300

    Added Task26

---
+++ Task26/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumOfNumbers = DigitOfNumbers(number);
+Console.WriteLine();
+
+
+int DigitOfNumbers(int num) 
+{
+    int digit = 0;
+    if (num == 0) return 1;
+    while (num != 0)
+    {
+        digit++;
+        num /= 10;
+    }
+    return digit;
+}
+
+
+Console.WriteLine($"Число {number} содержит {sumOfNumbers} цифр");
