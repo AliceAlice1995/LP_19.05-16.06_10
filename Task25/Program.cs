7bd4daad9120eb4c43f5ee96dc18fcd4a9e36749
Alisa Lubinets <lubinets.alisa@gmail.com>
2023-06-22 12:10:50 +0300

    Added Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+Console.WriteLine("Введите первое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите натуральное число: ");
+int step = Convert.ToInt32(Console.ReadLine());
+
+if (step <= 0)
+{
+    Console.WriteLine("Введено не натуральное число!");
+    return;
+}
+
+int res = CountStep(number, step);
+Console.WriteLine($"Число {number} в степени {step} = {res}");
+
+int CountStep(int num, int stepen)
+{
+    int result = 1;
+
+    for (int i = 1; i <= stepen; i++)
+    {
+        result *= num;
+    }
+
+    return result;
+}
