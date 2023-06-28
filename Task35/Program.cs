45029699f4e7eb53b40b77558d03ddb866baeb1f
Alisa Lubinets <lubinets.alisa@gmail.com>
2023-06-28 14:00:35 +0300

    Added Task35

---
+++ Task35/Program.cs
@@ -0,0 +1,11 @@
+﻿int UserInput(string str)
+{
+    Console.WriteLine(str);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int number = UserInput("Введите натуральное число: ");
+int number2 = UserInput("Введите положителбное число: ");
+int number3 = UserInput("Введите отрицателбное число: ");
+Console.WriteLine(number);
