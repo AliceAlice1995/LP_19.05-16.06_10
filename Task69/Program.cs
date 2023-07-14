9f115e79b3cc54ca7f84b4151cfd64d97228b4d0
Alisa Lubinets <lubinets.alisa@gmail.com>
2023-07-14 10:33:40 +0300

    Added Task69

---
+++ Task69/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+int EnterNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+} 
+
+int Exponentiation(int num, int exp)
+{
+    if (exp == 0) return 1;
+    return num * Exponentiation(num, exp - 1);
+}
+
+
+int number = EnterNumber("Введите целое число:);
+int exp = EnterNumber("Введите степень числа:");
+Console.WriteLine($"{exp}я степень числа {number} = {Exponentiation(number, exp)}");
